79de2e90a13c2213296c12aa9f0881296fb86af2
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-09-03 09:22:54 -0500

    working on pizza order

---
+++ src/main/java/PizzaOrderServlet.java
@@ -0,0 +1,13 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+
+@WebServlet(name= "PizzaOrderServlet", urlPatterns = "/pizza-order")
+public class PizzaOrderServlet extends HttpServlet{
+
+
+}
