1306470e071bc19ca502fb6228cfa973d0a74de3
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-09-03 15:58:11 -0500

    added getters and setters

--- src/main/java/Album.java
+++ src/main/java/Album.java
@@ -7,4 +7,53 @@ public class Album implements Serializable {
     private int releaseDate;
     private double sales;
     private String genre;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(int releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public double getSales() {
+        return sales;
+    }
+
+    public void setSales(double sales) {
+        this.sales = sales;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
 }
