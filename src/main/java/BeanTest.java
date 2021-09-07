f2c644476e551d8f3e35452f8beed1851e3e5c72
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-09-07 10:52:16 -0500

    changes

---
+++ src/main/java/BeanTest.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class BeanTest {
+    public static void main(String[] args) {
+        Album callMeIfYouGetLost = new Album();
+        callMeIfYouGetLost.setArtist("Tyler, the Creator");
+        callMeIfYouGetLost.setGenre("");
+    }
+}
