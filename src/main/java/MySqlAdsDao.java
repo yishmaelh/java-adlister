84bbffd91c9e02a18a39e23378b193659b4f4a71
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-09-09 13:57:45 -0500

    completed insert ad functionality

--- src/main/java/MySqlAdsDao.java
+++ src/main/java/MySqlAdsDao.java
@@ -2,30 +2,32 @@ import java.util.List;
 import java.sql.DriverManager;
 import java.sql.*;
 import java.util.ArrayList;
+
 import com.mysql.cj.jdbc.Driver;
 
-public class MySqlAdsDao implements Ads{
+public class MySqlAdsDao implements Ads {
     private Connection connection;
 
-    public MySqlAdsDao(Config config){
-        try{
+    public MySqlAdsDao(Config config) {
+        try {
             DriverManager.registerDriver(new Driver());
             connection = DriverManager.getConnection(
                     config.getUrl(),
                     config.getUser(),
                     config.getPassword()
             );
-        } catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
+
     @Override
     public List<Ad> all() {
         List<Ad> ads = new ArrayList<>();
-        try{
+        try {
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM ads");
-            while(resultSet.next()){
+            while (resultSet.next()) {
                 ads.add(new Ad(
                         resultSet.getLong("id"),
                         resultSet.getLong("user_id"),
@@ -33,7 +35,7 @@ public class MySqlAdsDao implements Ads{
                         resultSet.getString("description")
                 ));
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException("Error retrieving all ads.", e);
         }
         return ads;
@@ -41,7 +43,23 @@ public class MySqlAdsDao implements Ads{
 
     @Override
     public Long insert(Ad ad) {
-        return null;
+        long success;
+        try {
+            Statement stmt = connection.createStatement();
+            String query = String.format("INSERT INTO ads(user_id, title, description) VALUES(%d, '%s', '%s')",
+                    ad.getUserId(),
+                    ad.getTitle(),
+                    ad.getDescription()
+            );
+            System.out.println(query);
+            stmt.executeUpdate(query, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+            success = ad.getUserId();
+        } catch (SQLException e) {
+            throw new RuntimeException("Error creating new ad", e);
+        }
+        return success;
     }
 
 
