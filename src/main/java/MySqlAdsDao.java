3c3a32ecb7cee0518a3faba96671403d8497a3b7
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-09-09 10:32:46 -0500

    MySqlAdsDao

---
+++ src/main/java/MySqlAdsDao.java
@@ -0,0 +1,16 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import com.mysql.cj.jdbc.Driver;
+
+public class MySqlAdsDao implements Ads{
+    @Override
+    public List<Ad> all() {
+        return null;
+    }
+
+    @Override
+    public Long insert(Ad ad) {
+        return null;
+    }
+}
