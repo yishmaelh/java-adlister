64d75c6488f0b62ff928d163d02cda2e799ce638
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-09-13 09:42:18 -0500

    created config class, added method to hash password to RegisterServlet

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -2,6 +2,7 @@ package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.User;
+import org.mindrot.jbcrypt.BCrypt;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -21,6 +22,7 @@ public class RegisterServlet extends HttpServlet {
         String email = request.getParameter("email");
         String password = request.getParameter("password");
         String passwordConfirmation = request.getParameter("confirm_password");
+        String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
 
         // validate input
         boolean inputHasErrors = username.isEmpty()
